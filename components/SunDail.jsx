621347020c420b061b9b6b0d3db448c696a073de
rileykarldesign <rileykarldesign@email.com>
2023-11-10 18:06:44 +0000

    sun

--- components/SunDail.jsx
+++ components/SunDail.jsx
@@ -2,68 +2,12 @@ import React from 'react'
 
 export default function SunDail() {
   return (
-    <div className=' h-[100%] background-opacity-100 rounded-lg  bg-neutral-700  overflow-hidden relative'>
+    <div className=' h-[100%] background-opacity-100 rounded-lg  bg-blue-400  overflow-hidden relative'>
         
-    <div className="  absolute h-8 w-8 top-1 bg-amber-500  drop-shadow-lg -z-[-1] rounded-full  left-0 ">
+    <div className="  absolute h-8 w-8 top-1 left-1 bg-yellow-300  drop-shadow-[#fcba03] -z-[-1] rounded-full  left-0 ">
 
     </div>
 
-    <div className="zones h-[100%] w-full flex  ">
-
-
-    <div className=" zone w-[100%]  h-full border-r-[1px]  border-neutral-900 ">
-    </div>
-    <div className=" zone w-[100%]  h-full border-r-[1px]  border-neutral-900 ">
-    </div>
-    <div className=" zone w-[100%]  h-full border-r-[1px]  border-neutral-900 ">
-    </div>
-    <div className=" zone w-[100%]  h-full border-r-[1px]  border-neutral-900 ">
-    </div>
-    <div className=" zone w-[100%]  h-full border-r-[1px]  border-neutral-900 ">
-    </div>
-    <div className=" zone w-[100%]  h-full border-r-[1px]  border-neutral-900 ">
-    </div>
-    <div className=" zone w-[100%]  h-full border-r-[1px]  border-neutral-900 ">
-    </div>
-    <div className=" zone w-[100%]  h-full border-r-[1px]  border-neutral-900 ">
-    </div>
-    <div className=" zone w-[100%]  h-full border-r-[1px]  border-neutral-900 ">
-    </div>
-    <div className=" zone w-[100%]  h-full border-r-[1px]  border-neutral-900 ">
-    </div>
-    <div className=" zone w-[100%]  h-full border-r-[1px]  border-neutral-900 ">
-    </div>
-    <div className=" zone w-[100%]  h-full border-r-[1px]  border-neutral-900 ">
-    </div>
-    <div className=" zone w-[100%]  h-full border-r-[1px]  border-neutral-900 ">
-    </div>
-    <div className=" zone w-[100%]  h-full border-r-[1px]  border-neutral-900 ">
-    </div>
-    <div className=" zone w-[100%]  h-full border-r-[1px]  border-neutral-900 ">
-    </div>
-    <div className=" zone w-[100%]  h-full border-r-[1px]  border-neutral-900 ">
-    </div>
-    <div className=" zone w-[100%]  h-full border-r-[1px]  border-neutral-900">
-    </div>
-    <div className=" zone w-[100%]  h-full border-r-[1px]  border-neutral-900 ">
-    </div>
-    <div className=" zone w-[100%]  h-full border-r-[1px]  border-neutral-900 ">
-    </div>
-    <div className=" zone w-[100%]  h-full border-r-[1px]  border-neutral-900 ">
-    </div>
-    <div className=" zone w-[100%]  h-full border-r-[1px]  border-neutral-900 ">
-    </div>
-    <div className=" zone w-[100%]  h-full border-r-[1px]  border-neutral-900 ">
-    </div>
-    <div className=" zone w-[100%]  h-full border-r-[1px]  border-neutral-900 ">
-    </div>
-    <div className=" zone w-[100%]  h-full border-r-[1px]  border-neutral-900 ">
-    </div>
-    <div className=" zone w-[100%]  h-full border-r-[1px]  border-neutral-900 ">
-    </div>
-
-
-    </div>
 
 
     </div>
